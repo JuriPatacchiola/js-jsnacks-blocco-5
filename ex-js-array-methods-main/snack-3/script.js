a093e0cb8d3310a6d0efea5127ea7e1dd3d901c6
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-10-22 16:36:29 +0200

    snack with cicle for  classic

--- ex-js-array-methods-main/snack-3/script.js
+++ ex-js-array-methods-main/snack-3/script.js
@@ -1,4 +1,10 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
+const incrementedFor = [];
+for (let i = 0; i < numbers.length; i++) {
+  const num = numbers[i];       // prendo l’elemento corrente
+  incrementedFor.push(num + 1); // lo incremento e lo aggiungo al nuovo array
+}
+console.log(incrementedFor);
 // Risultato: [3, 9, 5, 8, 3, 88]

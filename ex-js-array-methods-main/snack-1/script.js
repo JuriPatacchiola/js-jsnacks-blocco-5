91889f7a11430ed243c92e6e750a8cf54b2b2cd3
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-10-22 16:31:40 +0200

    snack 1 compleate

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -1,4 +1,21 @@
-const names = ['Edoardo', 'Simone', 'Francesco'];
+
 
 // Stampa in console tutti i nomi
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+console.log("--- Ciclo 'for' con dichiarazione 'const' per i nomi ---");
+
+for (let i = 0; i < names.length; i++) {
+  // Dichiarazione 'const' per il nome corrente
+  const name = names[i]; 
+  
+  // Utilizzo della variabile 'name'
+  console.log(`Nome: ${name}`);
+}
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+//questo è con il foreach
+names.forEach(function(name, index) {
+
+  console.log(name);
+});
